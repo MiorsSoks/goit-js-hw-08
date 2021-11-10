531e9b51c74b0ddf9650b2a3555b76ef82b3187f
Myroslav Sokolov <mirossoks@gmail.com>
2021-11-10 18:33:13 +0200

    Сделал третье задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "feedback-form-state"
+const inputData = {}
+const form = document.querySelector('.feedback-form')
+
+function setItem(e) {
+    e.preventDefault();
+    inputData[e.target.name] = e.target.value;  
+     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputData));
+    // const formData = new FormData(form);
+    // formData.forEach((value, name) => console.log(value, name))
+}
+
+function populateForm() {
+    let savedInput = localStorage.getItem(LOCALSTORAGE_KEY)
+    if (savedInput) {
+        savedInput = JSON.parse(savedInput)
+        Object.entries(savedInput).forEach(([name, value]) => {
+            form[name] = value;
+            form.elements[name].value = value
+        })
+        
+    }
+}
+
+function submitForm(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    let objectInput = {}
+    formData.forEach((value, name) => objectInput[name] = value)
+    console.log(objectInput)
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    form.reset()
+
+}
+
+
+populateForm()
+
+form.addEventListener('input', throttle(setItem, 500))
+form.addEventListener('submit', submitForm)
